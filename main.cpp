a434cf07500fee6ec34132389256b7b600cdf4df
domaudluc <117200751+domaudluc@users.noreply.github.com>
2023-04-25 12:57:49 -0700

    Finished Product (for now) Missing features Add 2 more rooms * Done Font and display changes to make code easier to look at *For later Add commments explaining the purpose of most of the code *Done Errors Code loops mainmenu outputs over and over if any switch statements receive non integer*For later ^ if phone number is too long? *Fixed

--- main.cpp
+++ main.cpp
@@ -8,27 +8,17 @@ void title() // Function without parameter, without return type
     cout << "Hotel Management System" << endl;
     cout << "***********************" << endl;
 }
-class Room
+class Room      //Room Class to store data on hotel rooms
 {
 public:
-    string roomcat;
+    string roomcat;     //List of attributes for hotel rooms
     string booking;
     string guestname;
     string roomnum;
     int daysbooked;
-    int guestnum;
+    string guestnum; // Can't store more than 6 digits as int
 };
-
-int make_booking(){
-
-
-}
-int roomlegend()
-{
-}
-
 int roomcategories()
-
 {
     int roomdetails;
 
@@ -41,7 +31,6 @@ int roomcategories()
     cout << "3. Return to Main Menu" << endl;
     cout << "Enter the integer corresponding to the room details you wish to see or return to main menu: ";
     cin >> roomdetails;
-
     switch (roomdetails)
     {
         case 0:
@@ -64,23 +53,29 @@ int roomcategories()
             break;
         default:
             cout << "Invalid Input. Please Try Again" << endl;
-            roomcategories();
+            roomcategories(); // Loops room categories function so user can try again
+            // Code loops endlessly and breaks if letters are input
     }
 }
 int main()
 {
-    int choice,roomchoice;
-    Room Unit11A;
-    // Room Unit12B;
-    // Room Unit13C;
-    Unit11A.roomnum = "11A";
-    // Unit12B.roomnum = "12B";
-    // Unit13C.roomnum = "13C";
-    Unit11A.booking = "[Open]";
-    // Unit12B.booking = "Open";
-    // Unit13C.booking = "Open";
+    int menuchoice,roomchoice;
+    Room Unit11A;   // Declaring object in Room class
+    Room Unit12B;
+    Room Unit13C;
+    Unit11A.roomnum = "11A";    //Room number and booking status attributes assigned
+    Unit12B.roomnum = "12B";
+    Unit13C.roomnum = "13C";
+    Unit11A.booking = "Open";
+    Unit12B.booking = "Open";
+    Unit13C.booking = "Open";
     Unit11A.roomcat = "King Bed Deluxe";
-    do
+    Unit12B.roomcat = "2 Queen Beds Deluxe";
+    Unit13C.roomcat = "1 King Bed Deluxe with Sofa Sleeper";
+    Unit11A.daysbooked = 0; // days booked attribute assigned val zero for conditionals statement later
+    Unit12B.daysbooked = 0;
+    Unit13C.daysbooked = 0;
+    do              // Do while loop for main menu
     {
         title();
         cout << "0. Make Booking" << endl;
@@ -88,9 +83,9 @@ int main()
         cout << "2. Room Categories" << endl;
         cout << "3. Quit" << endl;
         cout << "Enter the integer corresponding to the menu you'd like to access: ";
-        cin >> choice;
+        cin >> menuchoice;
 
-        switch (choice)
+        switch (menuchoice)
         {
             case 0:
                 cout << "you selected make booking";
@@ -98,13 +93,13 @@ int main()
                 cout << "[Make Booking]" << endl;
                 cout << "Available Rooms" << endl;
                 cout << "0. " << Unit11A.roomnum <<" " << Unit11A.booking <<endl;
-                // cout << "1. " << Unit12B.booking;
-                // cout << "2. " << Unit13C.booking;
+                cout << "1. " << Unit12B.roomnum << " " << Unit12B.booking <<endl;
+                cout << "2. " << Unit13C.roomnum << " " << Unit13C.booking <<endl;
                 cout << "3. Return to Main Menu" << endl;
                 cout << "Please select a room: ";
                 cin >> roomchoice;
 
-                switch (roomchoice)
+                switch (roomchoice) //Switch statement tells code which case to run, each case corresponds to a different objet
                 {
                     case 0:
                         cout << "You selected unit 11A!" << endl;
@@ -117,12 +112,28 @@ int main()
                         cin >> Unit11A.daysbooked;
                         cout << "Booking Complete";
                         break;
-//        case 1:
-//            cout << "you selected int 12C";
-//            break;
-//        case 2:
-//            cout << "You selected unit 13C";
-//            break;
+                    case 1:
+                        cout << "You selected unit 12B!" << endl;
+                        Unit12B.booking = "[Closed]";
+                        cout << "Guest Last Name: ";
+                        cin >> Unit12B.guestname;
+                        cout << "Guest Phone Number: ";
+                        cin >> Unit12B.guestnum;
+                        cout << "Number of days booked: ";
+                        cin >> Unit12B.daysbooked;
+                        cout << "Booking Complete";
+                         break;
+                    case 2:
+                        cout << "You selected unit 13C!" << endl;
+                        Unit13C.booking = "[Closed]";
+                        cout << "Guest Last Name: ";
+                        cin >> Unit13C.guestname;
+                        cout << "Guest Phone Number: ";
+                        cin >> Unit13C.guestnum;
+                        cout << "Number of days booked: ";
+                        cin >> Unit13C.daysbooked;
+                        cout << "Booking Complete";
+                        break;
                     case 3:
                         cout << "You selected return to main menu";
                         break;
@@ -136,12 +147,42 @@ int main()
                 title();
                 cout << "[Room Legend]" << endl;
                 cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<< endl;
-                // Code for room 11A details
+                // Code to output object in room class attributes
                 cout << Unit11A.roomnum << endl;
                 cout << "Room Category: " <<  Unit11A.roomcat << endl;
                 cout << "Guest Last Name: " << Unit11A.guestname << endl;
                 cout << "Guest Contact Number: " << Unit11A.guestnum << endl;
-                cout << "Booked for " << Unit11A.daysbooked << " days" <<endl;
+                if (Unit11A.daysbooked == 0){
+                    cout << "Please navigate to [Make Booking] to reserve this open room"<<endl;
+                }
+                else
+                {
+                    cout << "Booked for " << Unit11A.daysbooked << " days" <<endl;
+                }
+                cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<< endl;
+                cout << Unit12B.roomnum << endl;
+                cout << "Room Category: " <<  Unit12B.roomcat << endl;
+                cout << "Guest Last Name: " << Unit12B.guestname << endl;
+                cout << "Guest Contact Number: " << Unit12B.guestnum << endl;
+                if (Unit12B.daysbooked == 0){
+                    cout << "Please navigate to [Make Booking] to reserve this open room"<<endl;
+                }
+                else
+                {
+                    cout << "Booked for " << Unit12B.daysbooked << " days" <<endl;
+                }
+                cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<< endl;
+                cout << Unit13C.roomnum << endl;
+                cout << "Room Category: " <<  Unit13C.roomcat << endl;
+                cout << "Guest Last Name: " << Unit13C.guestname << endl;
+                cout << "Guest Contact Number: " << Unit13C.guestnum << endl;
+                if (Unit13C.daysbooked == 0){
+                    cout << "Please navigate to [Make Booking] to reserve this open room" << endl;
+                }
+                else
+                {
+                    cout << "Booked for " << Unit13C.daysbooked << " days" <<endl;
+                }
                 cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<< endl;
                 break;
             case 2:
@@ -157,7 +198,7 @@ int main()
                 // Code loops endlessly and breaks if letters are input
         }
     }
-    while (choice!=4);
+    while (menuchoice!=4);
 
     return 0;
 }
